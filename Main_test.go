3ba0a6d25e05c86eca76f24c7770042905db188c
Danil Tkachenko <tkachwork13@gmail.com>
2020-06-04 00:41:28 +0300

    hotfix_test-print-result

--- Main_test.go
+++ Main_test.go
@@ -5,9 +5,8 @@ import (
 	"testing"
 )
 
-func TestPrintResult(t *testing.T)  {
-	printResult(100, 10, 10, 0.132323)
+func TestPrintResult(t *testing.T) {
+	printResult(100, 10, 10, 0.132323, 1)
 
 	require.True(t, true)
 }
-
